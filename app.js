303dbe48b27aa4ff8d07dea8057ef1ae866c4ce3
seiya1911 <n.siy1911cb@gmail.com>
2021-05-26 14:20:59 +0900

    [Add]app.css

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 $(function(){
-  $('body').html('<h1>Hello jQuery!!</h1>');
+  $('.box1').slideDown(function(){
+  $('.box1').css({
+    'background-color': '#0000FF',
+    'width': '200px',
+    'height': '100px'
+  }).slideUp();
+ });
 });
